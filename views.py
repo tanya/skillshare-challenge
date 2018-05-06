8ca50550347dc7845090bf6195dcacbd8f5fe705
tanya <tanyabalaraju@gmail.com>
2018-05-06 00:21:00 -0400

    cleaned up code, working on search

--- views.py
+++ views.py
@@ -5,42 +5,43 @@ import pytumblr
 client = pytumblr.TumblrRestClient('Bx7WX9YDTj9jn5ttururjm0a2OCIcOxc92u3fEHQho9NI6sKAE')
 client.info()
 app = Flask(__name__)
+pic_pages = [[]]
 
-@app.route('/', methods=['GET'])
-def home_redir():
-    return redirect(url_for('home', current=0))
-
-@app.route('/from-<current>', methods=['GET','POST'])
-def home(current):
-    current = int(current)
-    print ("Current value: {}".format(current))
-    posts = client.tagged('landscape')
-    photos = []
+@app.route('/', methods=['GET', 'POST'])
+def index(keyword=''):
+    """
+        - Gets images tagged with keyword (default: 'landscape') from Tumblr
+        - Divides API response into 10 responses per page
+        - Loads first page
+    """
+    if keyword == '': keyword = 'landscape'
+    posts = client.tagged(keyword)
+    photos, ind = [], 0
     for post in posts:
         if 'photos' not in post.keys(): continue
         for i in post['photos']:
             photos.append((i['original_size']['url']))
-    #print ("Number of results: {} ".format(len(photos)))
-    pic_pages = [[]]
-    ind = 0
+
     #10 results per page
     for i in range(len(photos)):
         pic_pages[ind].append(photos[i])
         if i % 10 == 0 and i != 0:
             ind += 1
             pic_pages.append([])
-    #current = 0
-    print ('Number of pages {}'.format(len(pic_pages)))
+    print (len(pic_pages))
+    return redirect(url_for('home', current=0))
+
+@app.route('/page=<current>', methods=['GET','POST'])
+def home(current):
+    """
+        - Handles subsequent requests
+    """
+    current = int(current)
+    maxm = len(pic_pages) - 1
+
     if request.method == 'GET':
-        return render_template('index.html', current=current, pics=pic_pages[current], maxm=len(pic_pages) - 1)
+        return render_template('index.html', current=current, pics=pic_pages[current], maxm=maxm)
+
     if request.method == 'POST':
-        if request.form.get('submit') == 'Next Page':
-            print ('Going to page {}'.format(current + 1))
-            current += 1
-            return render_template('index.html', current=current, pics=pic_pages[current if current < len(pic_pages) else current], \
-                    maxm = len(pic_pages) - 1)
-        elif request.form.get('submit') == 'Previous Page':
-            print ('Going to page {}'.format(current - 1))
-            current -= 1
-            return render_template('index.html', current=current, pics=pic_pages[current if current > 0 else current], \
-                    maxm = len(pic_pages) - 1)
+        #current is modified in frontend
+        return render_template('index.html', current=current, pics=pic_pages[current], maxm=maxm)
