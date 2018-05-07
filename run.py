629a120a71fcadfdb7ce117ab969f1297586bb4c
tanya <tanyabalaraju@gmail.com>
2018-05-07 19:52:00 -0400

    added lazy loading

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from views import app
-#app = Flask(__name__)
+import views
+from werkzeug import import_string, cached_property
+from flask import Flask
+from lazyview import LazyView
+
+app = Flask(__name__)
+app.add_url_rule('/', methods=['GET','POST'],
+                 view_func=LazyView('views.index'))
+app.add_url_rule('/page=<current>', methods=['GET','POST'],
+                 view_func=LazyView('views.home'))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=True)
--- views.py
+++ views.py
@@ -1,13 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
 import pytumblr
+from werkzeug import import_string, cached_property
 
 # Authenticate via API Key
 client = pytumblr.TumblrRestClient('Bx7WX9YDTj9jn5ttururjm0a2OCIcOxc92u3fEHQho9NI6sKAE')
 client.info()
-app = Flask(__name__)
+#app = Flask(__name__)
 pic_pages = [[]]
 
-@app.route('/', methods=['GET', 'POST'])
+"""
+class LazyView(object):
+    def __init__(self, import_name):
+        self.__name__ = import_name
+        self.import_name = import_name
+
+    @cached_property
+    def view(self):
+        return import_string(self.import_name)
+
+    def __call__(self, *args, **kwargs):
+        return self.view(*args, **kwargs)
+"""
+
+#@app.route('/', methods=['GET', 'POST'])
 def index(keyword=''):
     """
         - Gets images tagged with keyword (default: 'landscape') from Tumblr
@@ -44,7 +59,7 @@ def index(keyword=''):
     pic_pages = get_imgs('landscape')
     return redirect(url_for('home', current=0))
 
-@app.route('/page=<current>', methods=['GET','POST'])
+#@app.route('/page=<current>', methods=['GET','POST'])
 def home(current):
     """
         - Handles subsequent requests
@@ -53,3 +68,4 @@ def home(current):
     maxm = len(pic_pages) - 1
     #current is modified in frontend, for post
     return render_template('index.html', current=current, pics=pic_pages[current], maxm=maxm)
+
